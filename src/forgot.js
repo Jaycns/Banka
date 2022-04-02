d6a432a6afb80d8b1c250bb5f89aefcb4c2c6a89
Jaycn <okonkwojude69@gmail.com>
2022-04-02 07:22:43 +0100

    recent

--- src/forgot.js
+++ src/forgot.js
@@ -1,19 +1,19 @@
 import Banka from "./images/banka.png";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Loader from "./loader";
 import "./form.css";
 import { Link } from "react-router-dom";
 
 function Forgot() {
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
-  function handleLoader () {
-    setIsLoading(false)
+  function handleLoader() {
+    setIsLoading(false);
   }
-  
+
   return (
     <>
-     { isLoading ? <Loader handleLoader={handleLoader} /> : null }
+      {isLoading ? <Loader handleLoader={handleLoader} /> : null}
       <div className="biggie">
         <div className="logbox">
           <div className="wel">
@@ -48,7 +48,11 @@ function Forgot() {
                 <label className="labs">Email *</label>
               </div>
 
-              <a className="register-btn" onClick={() => setIsLoading(true)}>
+              <a
+                href="https://gogoanime.fi/"
+                className="register-btn"
+                onClick={() => setIsLoading(true)}
+              >
                 Reset
               </a>
             </div>
